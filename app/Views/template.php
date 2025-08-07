9a15f640e838bfb4ec37a608b01efc79f4d88190
Rencelagsil <lagsilrence@gmail.com>
2025-08-07 20:11:04 +0800

    added bootstrap fixed

--- app/Views/template.php
+++ app/Views/template.php
@@ -2,20 +2,20 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>CodeIgniter & Bootstrap</title>
+    <title>Bootstrap</title>
     <!-- Bootstrap 5 CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <nav class="navbar navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"> CodeIgniter 4 </a>
+            <a class="navbar-brand" href="#"> ITE311 </a>
         </div>
     </nav>
 
     <div class="container mt-5">
-        <h1 class="text-center">Welcome to CodeIgniter 4 + Bootstrap</h1>
-        <p class="text-center">This is a basic layout using Bootstrap.</p>
+        <h1 class="text-center">Welcome to ITE311 Homepage!</h1>
+        <p class="text-center">This is a homepage of ITE311 using basic bootstrap layout.</p>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
