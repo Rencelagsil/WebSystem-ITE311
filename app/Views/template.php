62aaf50eb69ef45390c619b86e4f8c6b57debe3c
Rencelagsil <lagsilrence@gmail.com>
2025-08-19 21:41:07 +0800

    some changes

--- app/Views/template.php
+++ app/Views/template.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Bootstrap</title>
+    <title>ITE311</title>
     <!-- Bootstrap 5 CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
